78afcc49d8e4c5474ec2aa8699b6cdc6e90cfc31
colinmiller <colinchmiller@gmail.com>
2015-10-22 16:41:56 -0500

    Update Style

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -27,12 +27,11 @@ function appendDom(data){
         $('.container').append('<div class="person well col-md-3"></div>');
         var $el = $('.container').children().last()
         //$el.addClass("count", count);
-        $el.append('<img src="' + data.people[i].imageURL + '">');
+        $el.append('<img src="' + data.people[i].imageURL + '"><br>');
         $el.append('<p>' + data.people[i].name + '</p>');
         $el.append('<p>' + data.people[i].location + '</p>');
         $el.append('<p>' + data.people[i].position + '</p>');
         $el.append('<button class="deleteme btn btn-danger">Delete</button>');
     }
-    //console.log(data.people.length);
-    //console.log(data);
+
 }
