5029bdb18e78745bdb872d6fce9a691d915580ee
colinmiller <colinchmiller@gmail.com>
2015-10-22 16:28:59 -0500

    Initial Commit

---
+++ server/public/scripts/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    $('.container').on('click', '.benjamin', function(){
+        $.ajax({
+            type: "GET",
+            url: "/data",
+            success: function(data){
+                appendDom(data);
+            }
+        });
+
+    })
+
+    $('.container').on('click', '.deleteme', function(){
+        $(this).parent().remove();
+    })
+
+
+
+
+});
+
+function appendDom(data){
+    for (var i=0; i<data.people.length; i++){
+
+
+        $('.container').append('<div class="person well col-md-3"></div>');
+        var $el = $('.container').children().last()
+        //$el.addClass("count", count);
+        $el.append('<img src="' + data.people[i].imageURL + '">');
+        $el.append('<p>' + data.people[i].name + '</p>');
+        $el.append('<p>' + data.people[i].location + '</p>');
+        $el.append('<p>' + data.people[i].position + '</p>');
+        $el.append('<button class="deleteme btn btn-danger">Delete</button>');
+    }
+    //console.log(data.people.length);
+    //console.log(data);
+}
